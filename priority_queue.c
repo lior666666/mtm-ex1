8bdd08096fc20e9669dab2b60f274defad116cbe
Lior Raskin <lior.raskin@campus.technion.ac.il>
2020-11-30 22:17:50 +0200

    fixed first 3 functions, added 3 new and another internal function

--- priority_queue.c
+++ priority_queue.c
@@ -44,7 +44,7 @@ PriorityQueue pqCreate(CopyPQElement copy_element, FreePQElement free_element, E
     return priority_queue;
  }
  /** Internal function, to create next element in our queue*/ 
-static PriorityQueue CreateNextInLine(PriorityQueue our_queue, PQElement element, PQElementPriority element_priority)
+static PriorityQueue createNextInLine(PriorityQueue our_queue, PQElement element, PQElementPriority element_priority)
 {
     PriorityQueue priority_queue = malloc(sizeof(PriorityQueue));
     if(priority_queue==NULL)
@@ -73,6 +73,27 @@ static PriorityQueue CreateNextInLine(PriorityQueue our_queue, PQElement element
     priority_queue->iterator = NULL;
     return priority_queue; 
 }
+//This will save us duplicated code. 
+static void insertElementPointer(PriorityQueue our_queue, PriorityQueue new_pointer) // This internal function inserts a spesific fully allocated new element to our queue. 
+{
+    PriorityQueue previous_element_in_queue = our_queue;
+    PriorityQueue current_element = our_queue->next_in_line;
+    while(current_element != NULL) //enter only if there is at least one real element in the queue
+    {
+        if(our_queue->comparePqElementPriority(current_element->pq_element_priority, new_pointer->pq_element_priority) < 0)
+        {
+            previous_element_in_queue->next_in_line = new_pointer;
+            new_pointer->next_in_line = current_element;
+            return;
+        }
+        previous_element_in_queue = current_element;
+        current_element = current_element->next_in_line;
+    }
+    //if there is no real elements in the queue OR new_priority is the lowest priority
+    current_element->next_in_line =  new_pointer;
+    new_pointer->next_in_line = NULL;
+}
+
  PriorityQueue pqCopy(PriorityQueue queue)
  {
     if(queue == NULL)
@@ -87,21 +108,21 @@ static PriorityQueue CreateNextInLine(PriorityQueue our_queue, PQElement element
         return NULL;
     }
      /** making temp pointers to go through the queue.*/ 
-    PriorityQueue temp_queue_pointer = queue->next_in_line;
+    queue = queue->next_in_line;
     PriorityQueue temp_copied_priority_queue_pointer = copied_priority_queue;
 
     /**Maybe there is a way to make the code shotrer and not duplicate*/
     
-    while(temp_queue_pointer!=NULL) 
+    while(queue!=NULL) 
     {
         /**copying the next one in the queue*/
-        temp_copied_priority_queue_pointer->next_in_line = CreateNextInLine(copied_priority_queue, temp_queue_pointer->pqe_element, temp_queue_pointer->pq_element_priority);
+        temp_copied_priority_queue_pointer->next_in_line = CreateNextInLine(copied_priority_queue, queue->pqe_element, queue->pq_element_priority);
         if(temp_copied_priority_queue_pointer->next_in_line==NULL)
         {
             pqDestroy(copied_priority_queue);
             return NULL;
         }
-        temp_queue_pointer = temp_queue_pointer->next_in_line;
+        queue = queue->next_in_line;
         temp_copied_priority_queue_pointer = temp_copied_priority_queue_pointer->next_in_line;
     }
     return copied_priority_queue;
@@ -126,22 +147,69 @@ static PriorityQueue CreateNextInLine(PriorityQueue our_queue, PQElement element
  }
 
 
-/** THIS FUNCTION NOT FINISHED YET!!!*/
 PriorityQueueResult pqChangePriority(PriorityQueue queue, PQElement element, PQElementPriority old_priority, PQElementPriority new_priority)
 {
     if(queue == NULL || element == NULL || old_priority == NULL || new_priority == NULL)
     {
         return PQ_NULL_ARGUMENT; 
     }
-    PriorityQueue temp_queue_pointer = queue->next_in_line;
-    while (temp_queue_pointer!=NULL)
+    PriorityQueue current_queue_pointer = queue->next_in_line;
+    PriorityQueue temp_queue_previous = queue;
+    while (current_queue_pointer!=NULL)  
     {
-        if(queue->equalPqElement(temp_queue_pointer->pqe_element, element) == true && queue->comparePqElementPriority(temp_queue_pointer->pq_element_priority,old_priority) == 0)
+        if(queue->equalPqElement(current_queue_pointer->pqe_element, element) == true && queue->comparePqElementPriority(current_queue_pointer->pq_element_priority,old_priority) == 0) 
         {
-            /** continue from here!*/ 
+            temp_queue_previous = temp_queue_previous->next_in_line->next_in_line;
+            current_queue_pointer->next_in_line = NULL; 
+            current_queue_pointer->freePqElementPriority(current_queue_pointer->pq_element_priority);
+            current_queue_pointer->copyPqElementPriority(new_priority);
+            if(current_queue_pointer->pq_element_priority==NULL)
+            {
+                return PQ_OUT_OF_MEMORY;
+            }
+            insertElementPointer(queue, current_queue_pointer);
+            return PQ_SUCCESS;
         }
+        current_queue_pointer = current_queue_pointer->next_in_line;
+        temp_queue_previous = temp_queue_previous->next_in_line;
     }
     return PQ_ELEMENT_DOES_NOT_EXISTS;
 }
 
+PriorityQueueResult pqRemoveElement(PriorityQueue queue, PQElement element)
+{
+    if(queue == NULL || element == NULL)
+    {
+        return PQ_NULL_ARGUMENT;
+    }
+    PriorityQueue current_queue_pointer = queue->next_in_line;
+    PriorityQueue previous_queue = queue;
+    while (current_queue_pointer!=NULL)
+    {
+        if(queue->equalPqElement(current_queue_pointer->pqe_element,element))
+        {
+            previous_queue = previous_queue->next_in_line->next_in_line;
+            queue->freePqElement(current_queue_pointer->pqe_element);
+            return PQ_SUCCESS;
+        }
+        current_queue_pointer = current_queue_pointer->next_in_line;
+        previous_queue = previous_queue->next_in_line; 
+    }
+    PQ_ELEMENT_DOES_NOT_EXISTS;
+}
+
+PQElement pqGetFirst(PriorityQueue queue)
+{
+    if(queue==NULL)
+    {
+        return NULL;
+    }
+    if(queue->next_in_line==NULL) // checks if the queue empty
+    {
+        return NULL; 
+    }
+    queue->iterator = queue->next_in_line; 
+    return queue->iterator; 
+}
+//We need to think in all function what if the user places int the function queue that starts not from the first element, because than all the generic functions will be NULL.
 
