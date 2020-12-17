4f7899c7c9322b3267373ae13229f6439911c05a
Lior Bar <liorbar03@gmail.com>
2020-12-17 22:12:44 +0200

    final version

--- date.c
+++ date.c
@@ -14,7 +14,15 @@ struct Date_t
     int year;
 };
 
-//*** 1 ***
+/**
+* dateCreate: Allocates a new date.
+* @param day - the day of the date.
+* @param month - the month of the date.
+* @param year - the year of the date.
+* @return
+* 	NULL - if allocation failed or date is illegal.
+* 	A new Date in case of success.
+*/
 Date dateCreate(int day, int month, int year)
 {
     if(day < 1 || day > DAYS_IN_MONTH)
@@ -36,7 +44,10 @@ Date dateCreate(int day, int month, int year)
     return new_date;
 }
 
-//*** 2 ***
+/**
+* dateDestroy: Deallocates an existing Date.
+* @param date - Target date to be deallocated. If priority queue is NULL nothing will be done
+*/
 void dateDestroy(Date date)
 {
     if (date != NULL)
@@ -45,7 +56,13 @@ void dateDestroy(Date date)
     }
 }
 
-//*** 3 ***
+/**
+* dateCopy: Creates a copy of target Date.
+* @param date - Target Date.
+* @return
+* 	NULL if a NULL was sent or a memory allocation failed.
+* 	A Date containing the same elements as date otherwise.
+*/
 Date dateCopy(Date date)
 {
     if (date == NULL)
@@ -63,7 +80,17 @@ Date dateCopy(Date date)
     return copy_date;
 }
 
-//*** 4 ***
+/**
+* dateGet: Returns the day, month and year of a date
+* @param date - Target Date
+* @param day - the pointer to assign to day of the date into.
+* @param month - the pointer to assign to month of the date into.
+* @param year - the pointer to assign to year of the date into.
+*
+* @return
+* 	false if one of pointers is NULL.
+* 	Otherwise true and the date is assigned to the pointers.
+*/
 bool dateGet(Date date, int* day, int* month, int* year)
 {
     if (date == NULL || day == NULL || month == NULL || year == NULL)
@@ -76,7 +103,13 @@ bool dateGet(Date date, int* day, int* month, int* year)
     return true;
 }
 
-//*** 5 ***
+/**
+* dateCompare: compares to dates and return which comes first
+* @return
+* 		A negative integer if date1 occurs first;
+* 		0 if they're equal or one of the given dates is NULL;
+*		A positive integer if date1 arrives after date2.
+*/
 int dateCompare(Date date1, Date date2)
 {
     if(date1 == NULL || date2 == NULL)
@@ -98,7 +131,10 @@ int dateCompare(Date date1, Date date2)
     return (date1->year)-(date2->year);
 }
 
-//*** 6 ***
+/**
+* dateTick: increases the date by one day, if date is NULL should do nothing.
+* @param date - Target Date
+*/
 void dateTick(Date date)
 {
     if (date != NULL)
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 compiler = gcc
-objs_em = event_manager.o date.o event.o event_manager_example_tests.o member.o priority_queue.o
-objs_pq = priority_queue.o pq_example_tests.o
+objs_em = event_manager.o date.o event.o event_manager_tests.o member.o priority_queue.o
+objs_pq = priority_queue.o priority_queue_tests.o
 COMP_FLAG = -std=c99 -Wall -pedantic-errors -Werror -DNDEBUG
 
 event_manager: $(objs_em)
@@ -13,11 +13,11 @@ event.o: event.c event.h priority_queue.h date.h member.h
 	$(compiler) -c $(COMP_FLAG) $*.c
 event_manager.o: event_manager.c event.h priority_queue.h date.h member.h event_manager.h
 	$(compiler) -c $(COMP_FLAG) $*.c
-event_manager_example_tests.o: event_manager_example_tests.c test_utilities.h event_manager.h date.h
+event_manager_tests.o: tests/event_manager_tests.c test_utilities.h event_manager.h date.h
 	$(compiler) -c $(COMP_FLAG) $*.c
 member.o: member.c member.h
 	$(compiler) -c $(COMP_FLAG) $*.c
-pq_example_tests.o: pq_example_tests.c test_utilities.h priority_queue.h
+priority_queue_tests.o: tests/priority_queue_tests.c test_utilities.h priority_queue.h
 	$(compiler) -c $(COMP_FLAG) $*.c
 priority_queue.o: priority_queue.c priority_queue.h
 	$(compiler) -c $(COMP_FLAG) $*.c
